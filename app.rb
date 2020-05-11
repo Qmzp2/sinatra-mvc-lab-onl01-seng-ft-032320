936cc99f2911634e8262fbae350482506df24ba5
Kevin Mathias <komathias1@gmail.com>
2020-05-11 21:10:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @user_phrase = params[:user_phrase]
+    @changed_phrase = 
+  end
 end
